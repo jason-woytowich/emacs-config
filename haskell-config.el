bfafd0ae0f1e9e22aa17c9f8820fa1d3ac7f5f6b
Jason Woytowich <jason.woytowich@gmail.com>
2015-06-13 18:48:55 -0700

    Haskell mode improvements
    - Flycheck for haskell enabled
    - Key sequence to access cabal file
    - Re-ordered projectile project type check.  If there is a .cabal it's a
      Haskell project regardless of wether or not there is a Makefile.

--- haskell-config.el
+++ haskell-config.el
@@ -1,6 +1,17 @@
+;;; haskell-config --- My haskell configuration
+;;; Commentary:
+;;; Code:
+
 (require 'haskell-mode)
+(require 'flycheck-haskell)
 
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'flycheck-mode)
+(add-hook 'flycheck-mode-hook 'flycheck-haskell-setup)
+
+(define-key haskell-mode-map (kbd "C-c v c") 'haskell-cabal-visit-file)
 
 (provide 'haskell-config)
+
+;;; haskell-config.el ends here
