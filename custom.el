371b72d82109563aae9a276f93b67ebcd62be685
Jason Woytowich <jwoytowich@apple.com>
2016-01-29 16:18:17 -0800

    Update many customs.

--- custom.el
+++ custom.el
@@ -10,26 +10,38 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-comphist-file "/Users/jwoytowich/.emacs.d/cache/ac-comphist.dat")
+ '(ag-reuse-buffers t)
  '(auto-save-list-file-prefix
    (expand-file-name "~/.emacs.d/cache/auto-save-list/.saves-"))
- '(c-default-style
+ '(c-default-style "stroustrup")
+ '(custom-safe-themes
    (quote
-    ((c-mode . "linux")
-     (c++-mode . "stroustrup")
-     (java-mode . "java")
-     (awk-mode . "awk")
-     (other . "gnu"))))
+    ("8453c6ba2504874309bdfcda0a69236814cefb860a528eb978b5489422cb1791" "cedd3b4295ac0a41ef48376e16b4745c25fa8e7b4f706173083f16d5792bb379" "419637b7a8c9cb43f273980f0c9879c0cbadace6b38efac0281e031772c84eb2" default)))
+ '(display-buffer-base-action
+   (quote
+    ((display-buffer--maybe-same-window display-buffer-reuse-window display-buffer--maybe-pop-up-frame-or-window display-buffer-in-previous-window display-buffer-use-some-window jfw-display-buffer-give-up-and-use-same-window))))
+ '(elfeed-feeds
+   (quote
+    ("http://feeds.gawker.com/lifehacker/full#_ga=1.181404135.1102956864.1439406278")))
  '(flycheck-clang-args nil)
  '(flycheck-puppet-parser-executable nil)
  '(global-git-gutter+-mode t)
  '(global-hl-line-mode t)
+ '(hexl-bits 8)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
+ '(magit-git-debug nil)
+ '(magit-push-current-set-remote-if-missing nil)
+ '(magit-status-headers-hook
+   (quote
+    (magit-insert-diff-filter-header magit-insert-head-header magit-insert-upstream-header)))
  '(make-backup-files nil)
  '(projectile-cache-file "/Users/jwoytowich/.emacs.d/cache/projectile.cache")
  '(projectile-known-projects-file "/Users/jwoytowich/.emacs.d/cache/projectile-bookmarks.eld")
  '(smartparens-global-mode t)
  '(sp-base-key-bindings (quote paredit))
+ '(tls-checktrust t)
+ '(visible-bell nil)
  '(whitespace-display-mappings (quote ((tab-mark 9 [187 9] [92 9]))))
  '(whitespace-style
    (quote
@@ -39,4 +51,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "nil" :family "Menlo")))))
+ )
