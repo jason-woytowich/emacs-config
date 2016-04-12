815d886b6bbc933b18eff84c0425a43e038a380b
Jason Woytowich <jwoytowich@apple.com>
2016-04-11 17:27:01 -0700

    Fixes to copy-with-file-and-line-numbers
    - Removed copy-with-line-numbers, never used it
    - New function 'relative-to-root', there has to be a library function
      for this somewhere.
    - Filenames are now relative to a root.  Use universal prefix to override.

--- jasons-helpers.el
+++ jasons-helpers.el
@@ -2,41 +2,19 @@
 ;;; Commentary:
 ;;; Todo:
 ;;;  - Language specific comments
-;;;  - Custom header information (project root, git tags/commits, etc.)
-;;;  - Be smarter about the size of the prepended line numbers, 5 digits does not look nice
+;;;  - Custom header information (git tags/commits, etc.)
 ;;;  - Copy as markup?
 ;;; Code:
 
-(defun copy-with-line-numbers (&optional begin end)
-  "Copies full lines covering the region BEGIN to END with line numbers prepended to each."
-  (interactive "r")
-
-  (unless (and begin end)
-    (error "Region not specified"))
-
-  (when (and begin end)
-    (save-excursion
-      (goto-char begin)
-      (move-beginning-of-line nil)
-      (setq begin (point))
-
-      (goto-char end)
-      (move-end-of-line nil)
-      (setq end (point))
-
-      (copy-region-as-kill begin end)
-
-      (let ((line (line-number-at-pos begin))
-            (end-line (line-number-at-pos end)))
-        (with-temp-buffer
-          (yank)
-          (goto-char 0)
-          (while (<= line end-line)
-            (move-beginning-of-line nil)
-            (insert (format "%5d: " line))
-            (setq line (+ 1 line))
-            (forward-line))
-          (copy-region-as-kill (point-min) (point-max)))))))
+(defun relative-to-root (root path)
+  "Return the relative path from ROOT to PATH."
+  (let ((abs-root (expand-file-name root))
+        (abs-path (expand-file-name path)))
+    (message abs-root)
+    (message abs-path)
+    (if (string-prefix-p abs-root abs-path)
+        (substring abs-path (length abs-root))
+      path)))
 
 (defun copy-with-file-and-line-numbers (&optional begin end)
   "Copies the region BEGIN to END with source filename and line numbers as a comment."
@@ -45,12 +23,14 @@
   (unless (and begin end)
     (error "Region not specified"))
 
-  (when (and begin end)
+  (let ((project-root (or
+                       (and (not current-prefix-arg) (projectile-project-root))
+                       "/")))
     (save-excursion
       (copy-region-as-kill begin end)
       (let ((line-start (line-number-at-pos begin))
             (line-end (line-number-at-pos end))
-            (file-name (buffer-file-name)))
+            (file-name (relative-to-root project-root (buffer-file-name))))
         (with-temp-buffer
           (insert (format "// %s, lines %d-%d\n" file-name line-start line-end))
           (yank)
